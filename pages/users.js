411d664042f81864e5612fb6f58f2d717f8b18a3
ShotAlex <shot.aleks@gmail.com>
2021-02-18 15:10:14 +0300

    feat: add fetch

--- pages/users.js
+++ pages/users.js
@@ -1,10 +1,26 @@
+import React, {useState, useEffect} from 'react';
 import Nav from "../components/Nav/Nav";
 
 const Users = () => {
+  const [users, setUsers] = useState([
+    { id:1, name:'' },
+  ])
+
+  useEffect(async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    setUsers(data)
+  }, [])
+
+  const userList = users.map( (user) =>
+    <li key={user.id}>User id: {user.id} | Name: {user.name}</li>
+  )
+
   return (
     <>
       <Nav />
       <h1>Users</h1>
+      {userList}
     </>
   );
 };
