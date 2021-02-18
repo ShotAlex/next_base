cd6cccd1310a7973be938dccff2852cfbd59ed5b
ShotAlex <shot.aleks@gmail.com>
2021-02-18 16:41:35 +0300

    feat: add mainContainer component

--- pages/users.js
+++ pages/users.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import MainContainer from "../components/MainContainer/MainContainer";
 
 const Users = ({users}) => {
+
   const userList = users.map( (user) => (
       <li key={user.id}>
         <Link href={`/users/${user.id}`}>
