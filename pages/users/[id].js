600b8fe6a8e89286d51823eb5a252b8ee95e3693
ShotAlex <shot.aleks@gmail.com>
2021-02-18 15:53:54 +0300

    feat: add single user

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,9 +1,20 @@
 import {useRouter} from "next/router";
 
-export default () => {
+export default ({user}) => {
   const {query} = useRouter()
 
   return (
-    <div> User id: {query.id}</div>
+    <>
+      <div> User id: {query.id}</div>
+      <div> Name: {user.name}</div>
+    </>
   )
+}
+
+export async function getServerSideProps({params}) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+  const user = await response.json()
+  return {
+    props: {user},
+  }
 }
