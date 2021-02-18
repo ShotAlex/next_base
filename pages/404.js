1d4f4f2d84bca28b10dcf919540d5da11895fdda
ShotAlex <shot.aleks@gmail.com>
2021-02-18 16:47:23 +0300

    feat: add 404 Not Found page

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainContainer from '../components/MainContainer/MainContainer';
+
+const NotFound = () => {
+    return (
+        <MainContainer>
+            <article>
+                Oooooops... Something went wrong
+            </article>
+        </MainContainer>
+    );
+};
+
+export default NotFound;
